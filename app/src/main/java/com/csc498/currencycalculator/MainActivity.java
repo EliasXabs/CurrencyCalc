695405d5757407fd89b0f6d3f8df609a6c5038de
EliasXabs <eliasxabs@gmail.com>
2022-09-10 16:58:03 +0300

    Added log in function
    Basic twists should be done to the UI for UX purposes (log in button too low. . . )

--- app/src/main/java/com/csc498/currencycalculator/MainActivity.java
+++ app/src/main/java/com/csc498/currencycalculator/MainActivity.java
@@ -3,6 +3,10 @@ package com.csc498.currencycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view){
+
+        EditText user_name = (EditText) findViewById(R.id.Userinput);
+        EditText password = (EditText) findViewById(R.id.Passinput);
+
+        if (user_name.getText().toString().equals("user.leb")
+                && password.getText().toString().equals("123456")){
+            Toast.makeText(getApplicationContext(), "Logged in", Toast.LENGTH_LONG).show();
+        }
+        else{
+            TextView error = (TextView) findViewById(R.id.Wrongcreds);
+            error.setVisibility(View.VISIBLE);
+        }
     }
 }
