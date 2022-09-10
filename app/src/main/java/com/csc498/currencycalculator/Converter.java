8d818bbb9c1097710473a619674b931a08e0d244
EliasXabs <eliasxabs@gmail.com>
2022-09-10 22:31:28 +0300

    Finished calculator one way
    To do:
    Convert vice versa
    To fix:
    Buttons not changing colors
    When the answer is more than 9 digits it gives an error (
        1- Might be because such numbers dont fit in float
        2- Something i dont even know about :D
    )

--- app/src/main/java/com/csc498/currencycalculator/Converter.java
+++ app/src/main/java/com/csc498/currencycalculator/Converter.java
@@ -3,6 +3,9 @@ package com.csc498.currencycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Converter extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class Converter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_converter);
+    }
+
+    public void convertusdlbp(View view){
+
+        TextView lbp_ele = (TextView) findViewById(R.id.results);
+        EditText usd_ele = (EditText) findViewById(R.id.toConvert);
+
+        float to_Convert = Float.parseFloat(usd_ele.getText().toString());
+        float converted = to_Convert*40000;
+
+        if (to_Convert < 250){
+            lbp_ele.setTextSize(50);
+        }
+        else{
+            lbp_ele.setTextSize(20);
+        }
+        lbp_ele.setText(Float.toString(converted));
     }
 }
